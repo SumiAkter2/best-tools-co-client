0ff93338b6d36c8cc99b78e4449de0fe5ab00f22
SumiAkter2 <akteralisha36@gmail.com>
2022-05-22 20:47:49 +0600

    adde purchase

--- src/Component/Navbar/Purchase.js
+++ src/Component/Navbar/Purchase.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Purchase = () => {
     return (
         <div>
-
+            <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+                <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        Shoes!
+                        <div class="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <div class="badge badge-outline">Fashion</div>
+                        <div class="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
