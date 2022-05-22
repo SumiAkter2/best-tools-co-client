0008ce4ad04b2fbe45465fb359fbb19f6de7e091
SumiAkter2 <akteralisha36@gmail.com>
2022-05-22 11:06:05 +0600

    added bannar  item

--- src/Component/Bannar.js
+++ src/Component/Bannar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import banner from '../asset/ronix-tools-Nn2mg1lUbZc-unsplash.jpg'
-
+import img1 from '../asset/tools-1.png'
 const Bannar = () => {
     return (
-        <div className='relative'>
-            <div class="hero lg:h-[70vh] bg-primary">
+        <div className='relative z-10'>
+            <div class="hero lg:h-[70vh] bg-primary ">
                 <div class="hero-content flex-col lg:flex-row">
 
                     <div className='max-w-6xl  text-left'>
@@ -12,15 +12,40 @@ const Bannar = () => {
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button class="btn btn-secondary">Get Started</button>
                     </div>
-                    <img src={banner} class="h-[50vh] rounded-md  shrink-0" alt='banner-image' />
+                    <img src={banner} class="h-[50vh]  rounded-md  shrink-0" alt='banner-image' />
                 </div>
             </div>
             {/* card item */}
-            <div>
+            <div className='rounded-2xl mx-auto mt-[-50px] relative z-20 bg-base-200 shadow-lg  w-3/4' >
+                <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4   '>
 
+
+                    <figure >
+                        <img style={{ 'width': '200px' }} src={img1} alt="Shoes" className='rounded-lg' />
+                        <h1>Driller</h1>
+                    </figure>
+
+
+
+                    <figure >
+                        <img style={{ 'width': '200px' }} src={img1} alt="Shoes" className='rounded - lg' />
+                        <h1>Driller</h1>
+                    </figure>
+
+                    <figure >
+                        <img style={{ 'width': '200px' }} src={img1} alt="Shoes" className='rounded-lg' />
+                        <h1>Driller</h1>
+                    </figure>
+
+                    <figure >
+                        <img style={{ 'width': '200px' }} src={img1} alt="Shoes" className='rounded-lg' />
+                        <h1>Driller</h1>
+                    </figure>
+
+                </div>
             </div>
 
-        </div>
+        </div >
     );
 };
 
