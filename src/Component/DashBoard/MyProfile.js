dc16b3b57b4cc839874576b6c776505bfe10952b
SumiAkter2 <akteralisha36@gmail.com>
2022-05-26 07:35:55 +0600

    work with my profile

--- src/Component/DashBoard/MyProfile.js
+++ src/Component/DashBoard/MyProfile.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import Spinner from '../Spinner';
+// import React, { useEffect, useState } from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import AllUser from './AllUser';
 
 const MyProfile = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch(`http://localhost:5000/user`, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
+
     return (
         <div>
-            <h1>Profile</h1>
+            <h2 className="text-2xl">All Users: {users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        {/* <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr> */}
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <AllUser
+                                key={user._id}
+                                user={user}
+                                refetch={refetch}
+                            ></AllUser>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
