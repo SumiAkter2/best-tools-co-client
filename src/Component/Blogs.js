b9edb9c99429896b305cbf4f5ee3194dbe67f0b5
SumiAkter2 <akteralisha36@gmail.com>
2022-05-26 18:17:11 +0600

    qs and ans

--- src/Component/Blogs.js
+++ src/Component/Blogs.js
@@ -9,7 +9,7 @@ const Blogs = () => {
                     1.How will you improve the performance of a React Application?
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans: Rapidly ship chat & in-app messaging with Stream's highly reliable chat infrastructure. Build scalable in-app chat or activity feeds in days. Get started with Stream today! Grow Your App.</p>
                 </div>
             </div>
 
@@ -19,7 +19,11 @@ const Blogs = () => {
                     2. What are the different ways to manage a state in a React application?
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans: There are four main types of state you need to properly manage in your React apps:
+                        Local state.
+                        Global state.
+                        Server state.
+                        URL state.</p>
                 </div>
             </div>
             <div className="collapse w-96 mx-auto mt-4 rounded-md">
@@ -28,7 +32,7 @@ const Blogs = () => {
                     3. How does prototypical inheritance work?
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans:The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
                 </div>
             </div>
             <div className="collapse w-96 mx-auto mt-4 rounded-md">
@@ -37,16 +41,16 @@ const Blogs = () => {
                     4. Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans: localStorage is a web storage object that allows JavaScript sites and apps to keep key-value pairs in a web browser with no expiration date.</p>
                 </div>
             </div>
             <div className="collapse w-96 mx-auto mt-4 rounded-md">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-neutral peer-checked:text-secondary-content">
-                    5. You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?
+                    5. localStorage is a web storage object that allows JavaScript sites and apps to keep key-value pairs in a web browser with no expiration date.
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans: localStorage is a web storage object that allows JavaScript sites and apps to keep key-value pairs in a web browser with no expiration date.</p>
                 </div>
             </div>
             <div className="collapse w-96 mx-auto mt-4 rounded-md">
@@ -55,7 +59,7 @@ const Blogs = () => {
                     6 What is a unit test? Why should write unit tests?
                 </div>
                 <div className="collapse-content bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>Ans: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia libero voluptas soluta ab fuga autem incidunt, suscipit consequuntur fugiat pariatur quisquam fugit voluptatibus, commodi, similique animi accusamus obcaecati dolores. Recusandae.</p>
+                    <p>Ans:localStorage is a web storage object that allows JavaScript sites and apps to keep key-value pairs in a web browser with no expiration date.</p>
                 </div>
             </div>
         </div>
